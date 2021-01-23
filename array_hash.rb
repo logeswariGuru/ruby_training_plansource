71a044cbd640b5e33a321a36f93903c8244c65c0
lguruswamy <lguruswamy@C02DFQ9WMD6M.local>
2021-01-23 01:18:15 -0800

    check_mark_by_name

--- array_hash.rb
+++ array_hash.rb
@@ -1,8 +1,21 @@
 class StudentArray 
-  
-  def initialize
-   student = [{name: "logu", roll_no: 1, marks: 100}, {name: "logesh", roll_no: 2, marks: 99}]
+  @@class_student = []
+  def initialize(stud_name, roll_num, marks)
+   @student = {"name": stud_name, "roll_no": roll_num, "marks": marks}
   end
-  
+
+  def insert
+  	@@class_student.push(@student)
+  end
+  # passing array of hash and name to check student pass or fail
+  def check_mark_by_name(class_student, name)
+  	class_student.each do |stud|
+   	 if stud[:name] == name
+  		@result = "pass" if stud[:marks] >= 35
+  		break
+  	 end
+  	end
+  	@result
+  end
 end
 
