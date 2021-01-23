1c141eb6b3ca336e558be33560445654fcd97d27
lguruswamy <lguruswamy@C02DFQ9WMD6M.local>
2021-01-23 01:56:08 -0800

    check mark with switch case

--- array_hash.rb
+++ array_hash.rb
@@ -17,5 +17,36 @@ class StudentArray
   	end
   	@result
   end
+
+  # using class varaible array of hash to to check student pass or fail based on name passed by user
+  def check_mark_by_name(name)
+  	@@class_student.each do |stud|
+   	 if stud[:name] == name
+  		@result = "pass" if stud[:marks] >= 35
+  		break
+  	 end
+  	end
+  	@result
+  end
+
+  # with switch case
+  def check_marks_by_use_case
+    @@class_student.each do |stud|
+   	 if stud[:name] == name
+   	 	marks = stud[:marks] 
+   	 	case marks
+   	    when marks > 80
+   	      "A"
+   	    when  marks >= 60 && marks < 80
+   	      "B"	
+   	    when  marks >= 40 && marks < 60
+   	      "C"
+  	    else
+  	 	  "D"		
+  	    end
+  	 end	
+   end
+  end
+
 end
 
