c14c5aac21219ddeb72a73f8878ad4c0298a6365
lguruswamy <lguruswamy@C02DFQ9WMD6M.local>
2021-01-24 01:18:40 -0800

    vehical with sub class access private

--- vehical.rb
+++ vehical.rb
@@ -1,18 +1,32 @@
 class Vehical
   attr_reader :no_of_tires, :total_seats
+
   def initialize(no_of_tires, total_seats)
     @no_of_tires = no_of_tires
     @total_seats = total_seats
   end
-  def moves
 
+  private
+  def moves(move)
+    p "#{move} with #{no_of_tires} tires and #{@total_seats} seats"
   end
 end
 
 class Car < Vehical
-
+  def initialize(no_of_tires, total_seats)
+	super
+	p "no_of_tires is #{no_of_tires}"
+  end
+  def move
+    moves("car moves safe")
+  end
 end
 
 class Bike < Vehical
-
+  def initialize(no_of_tires, total_seats)
+	super
+  end
+  def move
+    moves("car moves safe")
+  end
 end
